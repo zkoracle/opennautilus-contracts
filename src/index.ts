89718b41a8c76ae4fa4f099964a219efbe9002d8
ubinix-warun <warun@ubinix.com>
2024-01-05 17:19:55 +0700

    FIX: missing obj.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { Erc20 } from './Erc20Token.js';
+import { IERC20,
+    buildERC20Contract  } from './Erc20Token.js';
 import {
   IBasicTokenContract,
   buildBasicTokenContract,
 } from './BasicTokenContract.js';
 import { Toolkit } from './Toolkit.js';
 
-export { Erc20, Toolkit, IBasicTokenContract, buildBasicTokenContract };
+export { Toolkit, IBasicTokenContract, buildBasicTokenContract, IERC20, buildERC20Contract };
