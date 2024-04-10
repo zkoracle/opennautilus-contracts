a5176f47568c7b05c75bc1b75b2265857197d1dc
ubinix-warun <warun@ubinix.com>
2024-04-10 17:46:30 +0700

    Add 'last adoc 0.6.8'

--- src/token/Erc677Token.test.ts
+++ src/token/Erc677Token.test.ts
@@ -12,7 +12,7 @@ import {
   CircuitString,
   fetchAccount,
 } from 'o1js';
-import { IERC677, buildERC677Contract } from './Erc677Token.js';
+import { IERC677, buildERC677Contract, SErc677Contract } from './Erc677Token.js';
 
 const tokenSymbol = 'SOM';
 
@@ -21,10 +21,14 @@ let player1: PublicKey,
   //   player2: PublicKey,
   //   player2Key: PrivateKey,
   zkAppAddress: PublicKey,
-  zkAppPrivateKey: PrivateKey;
+  zkAppPrivateKey: PrivateKey,
+  serc677TokenAddress: PublicKey,
+  serc677TokenPrivateKey: PrivateKey;
 
 let tokenId: Field;
+let tokenSErc677Id: Field;
 let zkApp: SmartContract & IERC677;
+let zkAppSErc677: SErc677Contract;
 
 async function setupAccounts() {
   let Local = Mina.LocalBlockchain({
@@ -43,6 +47,13 @@ async function setupAccounts() {
 
   zkApp = await buildERC677Contract(zkAppAddress, 'SomeCoin', tokenSymbol, 9);
   tokenId = zkApp.token.id;
+
+  serc677TokenPrivateKey = PrivateKey.random();
+  serc677TokenAddress = serc677TokenPrivateKey.toPublicKey();
+
+  zkAppSErc677 = new SErc677Contract(serc677TokenAddress);
+  tokenSErc677Id = zkAppSErc677.token.id;
+
 }
 
 async function setupLocal() {
@@ -60,6 +71,17 @@ async function setupLocal() {
 }
 
 describe('Erc20 TokenContract', () => {
+
+  beforeAll(async () => {
+
+    console.log("beforeAll --- ")
+    SErc677Contract.staticSymbol = "PRC"
+    SErc677Contract.staticName = "PRICE"
+    SErc677Contract.staticDecimals = 9
+
+    await SErc677Contract.compile();
+  });
+
   beforeEach(async () => {
     await setupAccounts();
     await setupLocal();
@@ -79,5 +101,25 @@ describe('Erc20 TokenContract', () => {
     });
 
     // it.todo('building a valid token name on a token contract');
+
+    describe('Mint token', () => {
+      test('token contract can successfully mint with sign and updates the balances in the ledger', async () => {
+
+        expect(UInt64.zero).toEqual(zkAppSErc677.balanceOf(player1))
+
+        // Mint
+        const txnMint = await Mina.transaction(player1, () => {
+          AccountUpdate.fundNewAccount(player1);
+          zkAppSErc677.mint(player1, UInt64.from(500_000))
+        });
+
+        await txnMint.prove();
+        txnMint.sign([player1Key, serc677TokenPrivateKey]);
+        await txnMint.send();
+
+        expect(UInt64.from(500_000)).toEqual(zkAppSErc677.balanceOf(player1))
+
+      });
+    });
   });
 });
